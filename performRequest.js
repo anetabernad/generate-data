74878b7638de4f976fa58341baf0c57913d6c274
anetabernad <anetabernad@gmail.com>
2021-01-26 19:03:19 +0100

    add queue class

--- performRequest.js
+++ performRequest.js
@@ -16,116 +16,35 @@ function fireRequests(event) {
     offerTagsBody("jobsTagsQuantity"),
     setEnviromentUrl()
   );
-  const offersRequests = performRequest2(
-    "/offers",
-    offersBody("jobsQuantity"),
-    setEnviromentUrl()
-  );
-  const offersQueue = new PromiseQueue(offersRequests, 1); //tasks = an array of aysnc functions, 1 = number of tasks to run in parallel
-  offersQueue.run();
+  performRequest2("/offers", offersBody("jobsQuantity"), setEnviromentUrl());
 
-  const talentPoolsRequests = performRequest2(
+  performRequest2(
     "/offers",
     talentPoolsBody("talentpoolsQuantity"),
     setEnviromentUrl()
   );
-  const talentPoolsQueue = new PromiseQueue(talentPoolsRequests, 1);
-  talentPoolsQueue.run();
 
-  const disqualifyReasonsRequests = performRequest2(
+  performRequest2(
     "/disqualify_reasons",
     disqualifyReasonsBody("disqualifyReasonsQuantity"),
     setEnviromentUrl()
   );
 
-  const disqualifyReasonsQueue = new PromiseQueue(disqualifyReasonsRequests, 1);
-  disqualifyReasonsQueue.run();
-
-  const departmentsRequestes = performRequest2(
+  performRequest2(
     "/departments",
     departmentsBody("departmentsQuantity"),
     setEnviromentUrl()
   );
 
-  const departmentsQueue = new PromiseQueue(departmentsRequestes, 1);
-  departmentsQueue.run();
-
-  const candidatesRequestes = performRequest2(
+  performRequest2(
     "/candidates",
     candidatesBody("candidatesQuantity"),
     setEnviromentUrl()
   );
-
-  const candidatesQueue = new PromiseQueue(candidatesRequestes, 1);
-  candidatesQueue.run();
 }
-
-// Queue for offer
-function PromiseQueue(offersQueue = [], concurrentCount = 1) {
-  this.total = offersQueue.length;
-  this.todo = offersQueue;
-  this.running = [];
-  this.complete = [];
-  this.count = concurrentCount;
-}
-
-// Queue for talent pools
-function PromiseQueue(talentPoolsQueue = [], concurrentCount = 1) {
-  this.total = talentPoolsQueue.length;
-  this.todo = talentPoolsQueue;
-  this.running = [];
-  this.complete = [];
-  this.count = concurrentCount;
-}
-
-// Queue for disqualify reasons
-function PromiseQueue(disqualifyReasonsQueue = [], concurrentCount = 1) {
-  this.total = disqualifyReasonsQueue.length;
-  this.todo = disqualifyReasonsQueue;
-  this.running = [];
-  this.complete = [];
-  this.count = concurrentCount;
-}
-
-// Queue for departments
-function PromiseQueue(departmentsQueue = [], concurrentCount = 1) {
-  this.total = departmentsQueue.length;
-  this.todo = departmentsQueue;
-  this.running = [];
-  this.complete = [];
-  this.count = concurrentCount;
-}
-
-// Queue for candidates
-function PromiseQueue(candidatesQueue = [], concurrentCount = 1) {
-  this.total = candidatesQueue.length;
-  this.todo = candidatesQueue;
-  this.running = [];
-  this.complete = [];
-  this.count = concurrentCount;
-}
-
-PromiseQueue.prototype.runNext = function () {
-  return this.running.length < this.count && this.todo.length;
-};
-
-PromiseQueue.prototype.run = function () {
-  while (this.runNext()) {
-    const promise = this.todo.shift();
-    console.log("Run next");
-    console.log(this.todo.length);
-    promise.then(() => {
-      console.log("request done");
-      this.complete.push(this.running.shift());
-      this.run();
-    });
-    this.running.push(promise);
-  }
-};
 
 //Perform requests in queue (candidates, offers, tp, disqualify reasons, departments)
-function performRequest2(endpoint, data, enviromentUrl) {
-  const promises = [];
+const request = function performRequest2(endpoint, data, enviromentUrl) {
   for (i = 0; i < data.length; i++) {
     let body = JSON.stringify(data[i]);
 
@@ -146,11 +65,11 @@ function performRequest2(endpoint, data, enviromentUrl) {
       oReq.send(body);
     });
 
-    promises.push(requestPromise);
-  }
 
-  return promises;
+  }
 }
+
+
 
 //Perform request for tags, sources, offer tags
 function performRequest(endpoint, body, enviromentUrl) {
